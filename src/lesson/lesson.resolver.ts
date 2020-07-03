f093b39cdf98290f8e3e198f46055198582ca2df
Evgeniy Dubskiy <edubskiy@openwaygroup.com>
2020-07-03 19:35:18 +0200

    Added lesson service with createLesson method

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 
 @Resolver(of => LessonType)
@@ -12,4 +12,7 @@ export class LessonResolver {
       endDate: new Date().toISOString(),
     };
   }
+
+  @Mutation(returns => LessonType)
+  createLesson() {}
 }
