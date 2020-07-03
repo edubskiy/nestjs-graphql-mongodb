4a6bd09dc43c463cb3e149f52b39f79a7761da80
Evgeniy Dubskiy <edubskiy@openwaygroup.com>
2020-07-03 19:54:03 +0200

    Implemented get lesson by id feature

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -8,13 +8,8 @@ export class LessonResolver {
   constructor(private lessonService: LessonService) {}
 
   @Query(returns => LessonType)
-  lesson() {
-    return {
-      id: 'sdfjskfjskdf',
-      name: 'Physics class',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
   }
 
   @Mutation(returns => LessonType)
