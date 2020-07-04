15fb9c6e644a8eb4b610a0b595f3eb492da39246
Evgeniy Dubskiy <edubskiy@openwaygroup.com>
2020-07-04 10:05:36 +0200

    Inited all neccessary student files; created student entity

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { StudentService } from './student.service';
 
-@Module({})
+@Module({
+  providers: [StudentService]
+})
 export class StudentModule {}
