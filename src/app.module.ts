87c7af91bdd0509de8ca1a73926f45e8afb6c102
Evgeniy Dubskiy <edubskiy@openwaygroup.com>
2020-07-04 10:10:14 +0200

    Moved Student entity to correct file; added student entity to app and student modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
 import { Lesson } from './lesson/lesson.entity';
+import { Student } from './student/student.entity';
 import { StudentModule } from './student/student.module';
 
 @Module({
@@ -12,7 +13,7 @@ import { StudentModule } from './student/student.module';
       url: 'mongodb://localhost/school',
       synchronize: true,
       useUnifiedTopology: true,
-      entities: [Lesson],
+      entities: [Lesson, Student],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,7 +1,10 @@
+import { Student } from './student.entity';
 import { Module } from '@nestjs/common';
 import { StudentService } from './student.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  providers: [StudentService]
+  imports: [TypeOrmModule.forFeature([Student])],
+  providers: [StudentService],
 })
 export class StudentModule {}
